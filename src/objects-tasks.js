f751e0100e5f3b1cad34d0d62550a66e0a332eea
chekusheva <chekushevasofa1998@gmail.com>
2025-03-10 20:06:00 +0300

    feat: add new implementations

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -55,8 +55,16 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copy = {};
+  Object.assign(copy, obj);
+  const arrKeys = Object.keys(copy);
+  arrKeys.forEach((key) => {
+    if (keys.includes(key)) {
+      delete copy[key];
+    }
+  });
+  return copy;
 }
 
 /**
@@ -71,8 +79,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
